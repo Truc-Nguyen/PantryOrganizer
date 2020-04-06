a92b3a5f3c1d89c9a42d151f1ea471b09d85dc06
taylor-shi <52465564+taylor-shi@users.noreply.github.com>
2020-04-05 20:52:00 -0500

    pantry fragments set up

--- app/src/main/java/com/example/pantry_organizer/home.fragments/PantryFoodFragment.kt
+++ app/src/main/java/com/example/pantry_organizer/home.fragments/PantryFoodFragment.kt
@@ -1,22 +1,63 @@
 package com.example.pantry_organizer.home.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.pantry_organizer.R
+import com.example.pantry_organizer.data.PantryData
 import com.example.pantry_organizer.global.fragment.AbstractPantryAppFragment
+import com.example.pantry_organizer.home.adapters.PantryDataAdapter
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
+import kotlinx.android.synthetic.main.dialog_add_pantry.*
+import kotlinx.android.synthetic.main.fragment_food_list.*
+import kotlinx.android.synthetic.main.fragment_food_list.view.*
+import kotlinx.android.synthetic.main.fragment_pantry_list.*
 
 class PantryFoodFragment: AbstractPantryAppFragment() {
+    private var foodRecycler: RecyclerView? = null
+    private var mAdapter: PantryDataAdapter? = null
+
     // firebase instances already set in AbstractPantryAppFragment.
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         return inflater.inflate(R.layout.fragment_food_list, container, false)
+
+    }
+
+    override fun onStart(savedInstanceState: Bundle?) {
+        super.onStart()
+
+
+        foodRecycler = view!!.findViewById(R.id.food_recycler_view)
+
+        // Get users sorted by chips
+        val mQuery = db.collection("names")
+
+        // RecyclerView
+        mAdapter = object : PantryDataAdapter(mQuery) {
+        }
+
+        foodRecycler!!.setLayoutManager(LinearLayoutManager(this))
+        foodRecycler!!.setAdapter(mAdapter)
+
+        Log.d("recycler","set up w/ query ")
+
+        add_food_button.setOnClickListener{
+            //function to add new food item to database
+        }
+
+        back_button.setOnClickListener{
+            //go back to list of pantries
+        }
 
     }
 
--- app/src/main/java/com/example/pantry_organizer/home.fragments/PantryFragment.kt
+++ app/src/main/java/com/example/pantry_organizer/home.fragments/PantryFragment.kt
@@ -12,6 +12,7 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.pantry_organizer.R
+import com.example.pantry_organizer.data.PantryData
 import com.example.pantry_organizer.global.fragment.AbstractPantryAppFragment
 import com.example.pantry_organizer.home.adapters.PantryDataAdapter
 import com.google.firebase.auth.FirebaseAuth
@@ -20,6 +21,8 @@ import com.google.firebase.firestore.FirebaseFirestoreSettings
 import com.google.firebase.firestore.Query
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
+import kotlinx.android.synthetic.main.dialog_add_pantry.*
+import kotlinx.android.synthetic.main.fragment_food_list.view.*
 import kotlinx.android.synthetic.main.fragment_pantry_list.*
 
 class PantryFragment: AbstractPantryAppFragment() {
@@ -39,15 +42,18 @@ class PantryFragment: AbstractPantryAppFragment() {
 
         return inflater.inflate(R.layout.fragment_pantry_list, container, false)
 
+
+
     }
 
     override fun onStart(savedInstanceState: Bundle?) {
         super.onStart()
 
-        pantryRecycler =findViewById(R.id.pantry_recycler_view)
+
+        pantryRecycler = view!!.findViewById(R.id.pantry_recycler_view)
 
         // Get users sorted by chips
-        val mQuery = db.collection("names")
+        val mQuery = db.collection("pantry_names")
 
         // RecyclerView
         mAdapter = object : PantryDataAdapter(mQuery) {
@@ -66,28 +72,29 @@ class PantryFragment: AbstractPantryAppFragment() {
 
     private fun addPantryDialog() {
         // Opens the dialog view asking the user for
-        val dialogView = LayoutInflater.from(this.context).inflate(R.layout.enter_ace_value, null)
+        val dialogView = LayoutInflater.from(this.context).inflate(R.layout.dialog_add_pantry, null)
         val mBuilder = android.app.AlertDialog.Builder(this.context)
             .setView(dialogView)
-            .setTitle("Enter ace value: 1 or 11")
-            .setCancelable(false)
+            .setTitle("Enter new food location")
+            .setCancelable(true)
         val mAlertDialog = mBuilder.show()
 
         // Sets an onclick listener on the dialog box button
-        mAlertDialog.submitValue.setOnClickListener {
-            val inputValue = dialogView.ace_value.text.toString().toInt()
-            if(inputValue == 1 || inputValue == 11){
-                // updating player total here so that it correctly updates based on the chosen value
-                playerTotal+=inputValue
-                Log.d("playerTotal", playerTotal.toString())
-                //check if player busts
-                if (playerTotal>21) {
-                    endGame()
-                }
+        mAlertDialog.add_to_pantry.setOnClickListener {
+            val newLocation = dialogView.pantry_name.text.toString()
+
+            //need to check if pantry location already exists?
+            if(newLocation != ""){
+                //create new pantry object
+                val location = PantryData(newLocation)
+                // add new pantry location
+                db.collection("pantry_names").document(newLocation)
+                    .set(location)
+
                 mAlertDialog.dismiss()
             }else{
                 Toast.makeText(
-                    this, "You must input either 1 or 11",
+                    this.context, "Please enter pantry location",
                     Toast.LENGTH_SHORT
                 ).show()
             }
