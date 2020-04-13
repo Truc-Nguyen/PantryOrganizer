5e7dee8e375e7bb04151684fd22b082da513e30f
trucn <truc.nguyen@wustl.edu>
2020-04-13 18:14:01 -0500

    Cleaned up pantry list UI.
    - Truc

--- app/src/main/java/com/example/pantry_organizer/pantry/adapter/PantryListAdapter.kt
+++ app/src/main/java/com/example/pantry_organizer/pantry/adapter/PantryListAdapter.kt
@@ -1,6 +1,5 @@
 package com.example.pantry_organizer.pantry.adapter
 
-import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -8,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.pantry_organizer.R
@@ -20,7 +18,6 @@ import com.squareup.picasso.Picasso
 import jp.wasabeef.picasso.transformations.CropSquareTransformation
 import jp.wasabeef.picasso.transformations.RoundedCornersTransformation
 
-
 class PantryListAdapter(private val list: ArrayList<PantryData>?): RecyclerView.Adapter<PantryListViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PantryListViewHolder {
         val inflater = LayoutInflater.from(parent.context)
@@ -28,27 +25,25 @@ class PantryListAdapter(private val list: ArrayList<PantryData>?): RecyclerView.
     }
 
     override fun onBindViewHolder(holder: PantryListViewHolder, position: Int) {
-        val pantryName = list!![position].name
+        // Extract the pantry data from the position in the list.
+        val pantry = list?.get(position)
 
-        holder.bind(list!![position])
-        holder.itemView.setOnClickListener(object: View.OnClickListener{
-            override fun onClick(view: View?){
-                //            val myToast =
-//                Toast.makeText(this.context,"This would take you to food in the pantry", Toast.LENGTH_SHORT)
-//            myToast.show()
-                val activity = view!!.context as AppCompatActivity
-                var bundle = Bundle()
-                //is there a better identifier than pantry name?
-                Log.d("PantryName", pantryName)
-                bundle.putString("pantry_name", pantryName)
-                val fragment = PantryFoodFragment()
-                fragment.arguments = bundle
-                val transanction = activity.supportFragmentManager!!.beginTransaction()
-                transanction.replace(R.id.home_frameLayout, fragment)
-                transanction.commit()
-            }
-        })
+        // Bind the pantry data to the view holder.
+        holder.bind(pantry)
 
+        // todo ?
+        holder.itemView.setOnClickListener {
+            val activity = it!!.context as AppCompatActivity
+            var bundle = Bundle()
+            //is there a better identifier than pantry name?
+            Log.d("PantryName", pantry?.name)
+            bundle.putString("pantry_name", pantry?.name)
+            val fragment = PantryFoodFragment()
+            fragment.arguments = bundle
+            val transanction = activity.supportFragmentManager!!.beginTransaction()
+            transanction.replace(R.id.home_frameLayout, fragment)
+            transanction.commit()
+        }
     }
 
     override fun getItemCount(): Int {
@@ -58,26 +53,26 @@ class PantryListAdapter(private val list: ArrayList<PantryData>?): RecyclerView.
 
 class PantryListViewHolder(inflater: LayoutInflater, parent: ViewGroup):
 RecyclerView.ViewHolder(inflater.inflate(R.layout.adapter_pantry_list_item, parent, false)) {
-    //added to allow toasts to be made for testing in the bind function
-    private val context = parent.context
-
-    fun bind(pantryData: PantryData) {
+    fun bind(pantryData: PantryData?) {
+        // Get view objects.
         val pantryNameView: TextView = itemView.findViewById(R.id.adapter_pantryList_pantryName_textView)
         val pantryLocationView: TextView = itemView.findViewById(R.id.adapter_pantryList_pantryLocation_textView)
         val pantryImageView: ImageView = itemView.findViewById(R.id.adapter_pantryList_pantryImage_imageView)
 
-        pantryNameView.text = pantryData.name
-        pantryLocationView.text = pantryData.location
-        if (pantryData.imageLink == null) {
+        // Update view object data.
+        pantryNameView.text = pantryData?.name
+        pantryLocationView.text = pantryData?.location
+
+        // Update image view data.
+        if (pantryData?.imageLink == null) {
             pantryImageView.setImageResource(R.drawable.no_image_icon)
         } else {
             val imageRef = Firebase.storage.reference.child(pantryData.imageLink)
-
             imageRef.downloadUrl.addOnSuccessListener {
                 Picasso.get()
                     .load(it)
                     .transform(CropSquareTransformation())
-                    .transform(RoundedCornersTransformation(100, 0))
+                    .transform(RoundedCornersTransformation(25, 0))
                     .placeholder(R.drawable.loading_icon).into(pantryImageView)
             }.addOnFailureListener {
                 pantryImageView.setImageResource(R.drawable.no_image_icon)
