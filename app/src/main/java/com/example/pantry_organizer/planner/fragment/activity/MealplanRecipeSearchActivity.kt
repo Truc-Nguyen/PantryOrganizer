e4cd9097bd9c0ad21b5784d7dc4ca4a8c086fba2
taylor-shi <52465564+taylor-shi@users.noreply.github.com>
2020-04-23 21:05:27 -0500

    small subtitle change

--- app/src/main/java/com/example/pantry_organizer/planner/fragment/activity/MealplanRecipeSearchActivity.kt
+++ app/src/main/java/com/example/pantry_organizer/planner/fragment/activity/MealplanRecipeSearchActivity.kt
@@ -1,6 +1,8 @@
 package com.example.pantry_organizer.planner.fragment.activity
 
+import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.pantry_organizer.R
@@ -8,30 +10,35 @@ import com.example.pantry_organizer.data.RecipeData
 import com.example.pantry_organizer.global.activity.AbstractPantryAppActivity
 import com.example.pantry_organizer.planner.fragment.adapter.RecipeSearchAdapter
 import kotlinx.android.synthetic.main.activity_mealplan_recipe_search.*
+import java.time.LocalDate
 
 class MealplanRecipeSearchActivity: AbstractPantryAppActivity() {
     private var mealplanRecipeSearchList = ArrayList<RecipeData>()
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mealplan_recipe_search)
 
         // Extract the extras from intent.
-        val date = intent.extras!!.getString("MealplanDate")!!
+        val mealplanDate = intent.extras!!.getString("MealplanDate")!!
+
+        //convert date to readable view
+        val parsedDate = mealplanDate.split(".")
+        val date = LocalDate.of(parsedDate[2].toInt(), parsedDate[0].toInt(), parsedDate[1].toInt())
+        val weekDay = date.dayOfWeek.toString()
+        val correctDate = parsedDate[0] + "/" + parsedDate[1]
 
         // Support bar attributes.
         supportActionBar?.title = "Add to Pantry"
-        supportActionBar?.subtitle = date
+        supportActionBar?.subtitle = weekDay + " " + correctDate
         supportActionBar?.setHomeButtonEnabled(true)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+
         // Set up the recycler view to show api food search.
         val recyclerView = mealplanRecipeSearch_recyclerView
-        val adapter =
-            RecipeSearchAdapter(
-                mealplanRecipeSearchList,
-                date
-            )
+        val adapter = RecipeSearchAdapter(mealplanRecipeSearchList, mealplanDate)
         recyclerView.adapter = adapter
         recyclerView!!.layoutManager = LinearLayoutManager(this)
 
